ecdfe0ed6a6ea0e31e55a0d3cabb75f03ecf8afb
mauriciopj <mauricio.pj@outlook.com>
2017-07-18 10:52:03 -0300

    Aprimoramento das classes de entidade relacionadas #6

---
+++ Código Fonte/Aplicacao/src/br/edu/ifnmg/psc/sgo/aplicacao/PessoaFisica.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.sgo.aplicacao;
+
+/**
+ *
+ * @author mauricio
+ */
+public class PessoaFisica extends Pessoa {
+    protected String cpf;
+
+    public PessoaFisica() {
+        
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) throws ViolacaoRegraNegocioException {
+        if (!validaCPF(cpf))
+            throw new ViolacaoRegraNegocioException("CPF inválido!"); 
+        this.cpf = cpf;
+    }  
+    
+    private static final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+       
+    public static boolean validaCPF(String cpf) throws ViolacaoRegraNegocioException {
+        if (cpf==null || cpf.length()!=11)
+            throw new ViolacaoRegraNegocioException("O CPF deve conter 11 digitos!");
+
+        Integer digito1 = calcularDigito(cpf.substring(0,9), pesoCPF);
+        Integer digito2 = calcularDigito(cpf.substring(0,9) + digito1, pesoCPF);
+        return cpf.equals(cpf.substring(0,9) + digito1.toString() + digito2.toString());
+   }
+}
---
+++ Código Fonte/Aplicacao/src/br/edu/ifnmg/psc/sgo/aplicacao/PessoaJuridica.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.sgo.aplicacao;
+
+/**
+ *
+ * @author mauricio
+ */
+public class PessoaJuridica extends Pessoa {
+    protected String cnpj;
+
+    public PessoaJuridica() {
+        
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }        
+    
+    public void setCnpj(String cnpj) throws ViolacaoRegraNegocioException {
+        if (!validaCNPJ(cnpj))
+            throw new ViolacaoRegraNegocioException("CNPJ inválido!"); 
+        this.cnpj = cnpj;
+    }
+        
+    private static final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+    public static boolean validaCNPJ(String cnpj) throws ViolacaoRegraNegocioException {
+        if (cnpj==null || cnpj.length()!=14)
+            throw new ViolacaoRegraNegocioException("O CNPJ deve conter 14 digitos!");
+        
+        Integer digito1 = calcularDigito(cnpj.substring(0,12), pesoCNPJ);
+        Integer digito2 = calcularDigito(cnpj.substring(0,12) + digito1, pesoCNPJ);
+        return cnpj.equals(cnpj.substring(0,12) + digito1.toString() + digito2.toString());
+    }        
+}
